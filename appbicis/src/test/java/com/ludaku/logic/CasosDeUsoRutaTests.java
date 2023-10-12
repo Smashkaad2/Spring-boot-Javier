d6e40b2c260676f68f2e53281fbef8c00b4510cb
Jaime Chavarriaga <jaime.chavarriaga@gmail.com>
2023-10-12 00:48:13 +0000

    Ajusta pruebas de Crear Rutas
    - Agrega configuración para la prueba
    - Agrega asserts para las pruebas

--- appbicis/src/test/java/com/ludaku/logic/CasosDeUsoRutaTests.java
+++ appbicis/src/test/java/com/ludaku/logic/CasosDeUsoRutaTests.java
@@ -1,15 +1,22 @@
 package com.ludaku.logic;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.ludaku.appbicis.AppbicisApplication;
 import com.ludaku.appbicis.logic.CasosDeUsoCrearRutas;
+import com.ludaku.appbicis.modelo.Ruta;
 import com.ludaku.appbicis.modelo.Usuario;
+import com.ludaku.appbicis.repositorios.RepositorioRuta;
 import com.ludaku.appbicis.repositorios.RepositorioUsuario;
 
-@SpringBootTest
+@SpringBootTest(classes = AppbicisApplication.class)
 public class CasosDeUsoRutaTests {
 
     @Autowired
@@ -18,14 +25,40 @@ public class CasosDeUsoRutaTests {
     @Autowired
     RepositorioUsuario usuarios;
 
+    @Autowired
+    RepositorioRuta rutas;
+
+    @BeforeEach
+    public void borraBD() {
+
+        rutas.deleteAll();
+        usuarios.deleteAll();
+
+    }
+
     @Test
     public void pruebaCrearRuta() {
-
-        usuarios.deleteAll();
-        Usuario u = new Usuario("Javier");
-        u = usuarios.save(u);
+        
         try {
+
+            // Arrange
+            Usuario u = new Usuario("Javier");
+            u = usuarios.save(u);
+    
+            // act
             pruebaRutaCrear.crearRutas("Casa", "Ruta A casa", u.getUsuarioID());
+
+            // assert
+            List<Ruta> listaRutas = rutas.findByNombreRuta("Casa");
+            assertTrue(listaRutas.size() > 0, "No se grabó la ruta");
+
+            Ruta rutaEnBD = listaRutas.get(0);
+            assertNotNull(rutaEnBD, "La ruta es NULL");
+            assertEquals(rutaEnBD.getDescripcion(), "Ruta A casa", "La descripción no coincide");
+            assertNotNull(rutaEnBD.getAutor(), "La ruta tiene el autor en NULL");
+            assertEquals(rutaEnBD.getAutor().getNombreU(), "Javier", "El nombre del autor no coincide");
+
+
         } catch (Exception e) {
             fail("El programa fallo y no deberia");
         }
@@ -35,12 +68,19 @@ public class CasosDeUsoRutaTests {
     @Test
     public void pruebaCrearRutaConUsuarioInexistente() {
 
-        usuarios.deleteAll();
         try {
+
             pruebaRutaCrear.crearRutas("Casa", "Ruta A casa", 10L);
+            
             fail("El programa creo la ruta y no deberia porque el usuario no existe");
+
         } catch (Exception e) {
-            //Ok
+
+            // valida que no se halla creado una ruta con ese nombre
+            List<Ruta> listaRutas = rutas.findByNombreRuta("Casa");
+            assertTrue(listaRutas.size() == 0, "Se grabó una ruta y no debía");
+
+            // Ok
         }
 
     }
